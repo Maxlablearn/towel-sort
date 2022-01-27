4824c53bf4c4820fe63bc9b0fa6846fcaf2a8d6d
MaxLabLearn <maxlablearn@gmail.com>
2022-01-27 09:00:45 +0300

    towel sort task complited

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  if (matrix == undefined) {
+    return [];
+  } if ( matrix === []) {
+    return [];
+  }
+    return matrix.map((a, b) => b % 2 === 0 ? a : a.reverse()).flat();
 }
